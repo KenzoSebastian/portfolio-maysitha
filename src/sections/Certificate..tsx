99918fc3d07a3af4b68b348f6057fa33949cdc26
Kenzo Sebastian <kenzosebastian4@gmail.com>
2026-01-04 22:55:05 +0700

    feat: view more

--- src/sections/Certificate..tsx
+++ src/sections/Certificate..tsx
@@ -1,5 +1,8 @@
 import { HeaderSection } from "@/components/HeaderSection.";
+import GradualBlur from "@/components/motion/GradualBlur";
 import { PdfOverlay } from "@/components/PdfOverlay";
+import { Button } from "@/components/ui/button";
+import { ChevronDown } from "lucide-react";
 import { motion } from "motion/react";
 import { useState } from "react";
 
@@ -71,9 +74,13 @@ const certificates = [
 
 export const CertificateSection = () => {
   const [selectedPdf, setSelectedPdf] = useState<string | null>(null);
+  const [collapsed, setCollapsed] = useState(true);
 
   return (
-    <section className="section" id="certificates">
+    <section
+      className={`section ${collapsed ? "max-h-225 overflow-hidden" : "max-h-auto"}`}
+      id="certificates"
+    >
       <HeaderSection
         title="Certifications"
         subTitle="A testament to professional training, excellence, and dedication in the modeling and creative industry."
@@ -109,8 +116,12 @@ export const CertificateSection = () => {
         ))}
       </div>
 
-      {selectedPdf && (
-        <PdfOverlay selectedPdf={selectedPdf} setSelectedPdf={setSelectedPdf} />
+      {selectedPdf && <PdfOverlay selectedPdf={selectedPdf} setSelectedPdf={setSelectedPdf} />}
+      {collapsed && (
+        <div className="absolute bottom-0 left-0 right-0">
+          <GradualBlur position="bottom" strength={3} target="parent" zIndex={1} />
+          <Button onClick={() => setCollapsed(false)} className="z-2 w-20 h-6 sm:w-23 sm:h-8 md:w-25 md:h-9 text-[9px] sm:text-[11px] md:text-[13px] flex justify-center items-center absolute bottom-4 left-1/2 -translate-x-1/2 rounded-full opacity-70 hover:opacity-100"><ChevronDown className="w-7 h-7 sm:w-9 sm:h-9 md:w-15 md:h-15" /> view more</Button>
+        </div>
       )}
     </section>
   );
