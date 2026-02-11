4231f6855618d3291c7d748ab23819ff9a7d0733
Kenzo Sebastian <kenzosebastian4@gmail.com>
2026-02-11 17:14:45 +0700

    optimizing: speedInsights Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,18 @@ import App from "./App.tsx";
 import { ThemeProvider } from "../components/ThemeProvider.tsx";
 import { MotionConfig } from "motion/react";
 import { Toaster } from "sonner";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <MotionConfig transition={{ duration: 0.5, ease: "easeInOut", bounce: 0.4, type: "spring", delay: 0.2 }}>
+      <MotionConfig
+        transition={{ duration: 0.5, ease: "easeInOut", bounce: 0.4, type: "spring", delay: 0.2 }}
+      >
         <App />
         <Toaster position="top-right" />
+        <SpeedInsights />
       </MotionConfig>
     </ThemeProvider>
-  </StrictMode>
+  </StrictMode>,
 );
